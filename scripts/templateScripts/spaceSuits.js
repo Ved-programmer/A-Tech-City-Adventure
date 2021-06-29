a93499f586c342e8d7dd13091eaabbe0d6a31756
Ved Rathi <65692297+Ved-programmer@users.noreply.github.com>
2021-06-29 10:45:38 +0530

    Create spaceSuits.js

---
+++ scripts/templateScripts/spaceSuits.js
@@ -0,0 +1,25 @@
+var templateItems = {
+    "Explorer's Suit" : {
+        "energySource":["Bio Mass"], 
+        "spaceAirport":[], 
+        "spaceShip":[], 
+        "spaceSuits":[], 
+        "weapons":[]
+    },
+
+    "Water Repellent" : {
+        "energySource":["Hydraulic Power"], 
+        "spaceAirport":["aqua aerodrome"], 
+        "spaceShip":["Sailing Ship"], 
+        "spaceSuits":[], 
+        "weapons":[]
+    }, 
+
+    "Precaution Piece" : {
+        "energySource":["Electrical Energy"], 
+        "spaceAirport":["warm cosmos"], 
+        "spaceShip":[], 
+        "spaceSuits":[], 
+        "weapons":[]
+    }
+};
